1e57f7707deb5500d39e47acae5ae735484a6bb0
TatianaBortnik <tanya@bortnik.org>
2023-03-03 01:24:31 +0200

    finished project for homework week1

---
+++ week1/project/ecommerce/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+import categories from './fake-data/all-categories';
+import products from './fake-data/all-products';
+import CategoriesList from './components/Categories';
+import ProductsList from './components/Products';
+
+function App() {
+  const [state, setState] = useState({selectedCategory:'', isAll: true})
+  return (
+    <div className="App">
+      <h1>Products</h1>
+      { CategoriesList({categories, state, setState}) }
+      { ProductsList({products, state}) }
+    </div>
+  );
+}
+
+export default App;
