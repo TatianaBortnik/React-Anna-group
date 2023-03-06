ea078fd9a006461f1264a69c23d4aa0f1c0de0eb
TatianaBortnik <tanya@bortnik.org>
2023-03-06 11:19:30 +0200

    Perfect service - images moved from public

--- week1/possible-qa-exercises/2-perfect-customer-service/src/data/guarantee.js
+++ week1/possible-qa-exercises/2-perfect-customer-service/src/data/guarantee.js
@@ -1,20 +1,25 @@
-export default [
+import delivery from '../images/f-delivery.png';
+import coin from '../images/coin.png';
+import chat from '../images/chat.png';
+
+const data = [
     {
         id: 1,
-        img: '/assets/f-delivery.png',
+        img: delivery,
         title: 'Free shipping',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     },
     {
         id: 2,
-        img: '/assets/coin.png',
+        img: coin,
         title: '100% Money back',
         description: 'Dictum non consectetur a erat nam at lectus urna duis. Laoreet sit amet cursus sit amet dictum sit amet justo. '
     },
     {
         id: 3,
-        img: '/assets/chat.png',
+        img: chat,
         title: 'Online support 24/7',
         description: 'Condimentum vitae sapien pellentesque habitant morbi. Quam nulla porttitor massa id neque. '
     }
 ]
+export default data;
